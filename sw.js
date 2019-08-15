cb10d0983c38672b6a3b60a9052b9449cc687f60
jackfader <jacksfader@gmail.com>
2019-08-14 22:28:20 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,20 +2,20 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('PizzaPy').then(function(cache) {
      return cache.addAll([
-       '/index.html',
-       '/custom.css',
-       '/manifest1.json',
-       '/sw.js',
-       '/browserconfig.xml',
-       'img/apple-touch-icon.png',
-       'img/android-chrome-192x192.png',
-       'img/android-chrome-512x512.png',
-       'img/favicon-16x16.png',
-       'img/favicon-32x32.png',
-       'img/favicon.ico',
-       'img/mstile-150x150.png',
-       'img/pizza.png',
-       'img/safari-pinned-tab.svg'
+       './index.html',
+       './custom.css',
+       './manifest1.json',
+       './sw.js',
+       './browserconfig.xml',
+       './img/apple-touch-icon.png',
+       './img/android-chrome-192x192.png',
+       './img/android-chrome-512x512.png',
+       './img/favicon-16x16.png',
+       './img/favicon-32x32.png',
+       './img/favicon.ico',
+       './img/mstile-150x150.png',
+       './img/pizza.png',
+       './img/safari-pinned-tab.svg'
      ]);
    })
  );
